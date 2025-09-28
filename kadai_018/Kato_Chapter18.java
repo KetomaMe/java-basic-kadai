3e84c6f08a661cb946d8016124b0dbde1383e332
KetomaMe <goingxboyz@gmail.com>
2025-09-28 15:24:16 +0900

    Add files via upload

---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,17 @@
+package kadai_018;
+
+public abstract class Kato_Chapter18 {
+	public String familyName;
+	public String givenName;
+	public String address;
+	
+	public void commonIntroduce() {
+		
+	}
+	public abstract void eachIntroduce();
+	
+	public void execIntroduce() {
+		
+	}
+
+}
